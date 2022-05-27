a79aea19968b9e580e9d38afa334413a9e120c40
Konstantin Zhidkov <konstantin.zhidkov1997@gmail.com>
2022-05-28 01:30:16 +0200

    Creating Feed component and globalFeed page to render articles.

--- src/pages/globalFeed/globalFeed.js
+++ src/pages/globalFeed/globalFeed.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
+import useFetch from "hooks/useFetch";
+import Feed from "components/Feed/Feed";
+
 
 const GlobalFeed = () => {
-    return <div>Global Feed</div>
+    const apiUrl = '/articles?limit=10&offset=0'
+    const [{response, isLoading, error}, doFetch] = useFetch(apiUrl);
+    console.log(response);
+
+    React.useEffect(() => {
+        doFetch();
+    }, [doFetch]);
+
+    return (
+        <div className="home-page">
+            <div className="banner">
+                <div className="container">
+                    <h1>Header</h1>
+                    <p>Description</p>
+                </div>
+            </div>
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-9">
+                        {isLoading && <div>Loading...</div>}
+                        {error && <div>Something went wrong</div>}
+                        {!isLoading && response && (
+                            <Feed articles={response.articles}></Feed>
+                        )}
+                    </div>
+                    <div className="col-md-3">
+                        Popular tags
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default GlobalFeed;
